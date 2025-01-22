ed667f715e937befc9e90a62ed6c70b445e97d92
kiya kebe <kiyakebe799@gmail.com>
2025-01-22 23:14:18 +0300

    fix: deploy error

--- src/app/(main)/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProductList from "@/components/product/ProductList";
-import { useRouter } from "next/router";
-
-export default function Home() {
-  const router = useRouter();
-  router.push("/");
-
-  return <ProductList />;
-}
